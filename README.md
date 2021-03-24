d28bb28f709e74253aa3eab65f6a2597b8c6ba8d
metoonhathung <nhathungmetoo@gmail.com>
2021-03-25 03:41:06 +0700

    basic functions

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 https://metoonhathung.github.io/virtualassistant/
 
+Description: Virtual Assistant with voice/text input/output. Utilize APIs like Web Speech, Wolfram Alpha, Wikipedia, OpenWeatherMap, Oxford Dictionary, YouTube, Google Maps, Google Translate, Joke, MapQuest. Able to answer questions, search articles, check weather, search dictionary, play videos, show maps, translate texts, search google, tell jokes, show location, show time, show date.
+
+Technologies: Javascript, AJAX, HTML, CSS, Bootstrap
+
 ### To run the code locally:
 
 ```
--- script.js
+++ script.js
@@ -1,83 +1,313 @@
-const speak = document.querySelector("#speak");
-const you = document.querySelector("#you");
-const bot = document.querySelector("#bot");
+const speakButton = document.querySelector("#speakButton");
+const executeButton = document.querySelector("#executeButton");
+const textInput = document.querySelector("#textInput");
+const youIntent = document.querySelector("#youIntent");
+const youQuery = document.querySelector("#youQuery");
+const botAnswer = document.querySelector("#botAnswer");
+const embeddedDiv = document.querySelector("#embeddedDiv");
+let supported = false;
+let start = false;
+let recognition, synth, utter;
 
-const recognition = new webkitSpeechRecognition();
-recognition.continuous = false;
-recognition.lang = "en-US";
-recognition.interimResults = false;
-recognition.maxAlternatives = 1;
+if ("SpeechRecognition" in window || "webkitSpeechRecognition" in window) {
+  supported = true;
+  speakButton.classList.remove("disabled");
+  const SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+  recognition = new SpeechRecognition();
+  recognition.continuous = false;
+  recognition.lang = "en-US";
+  recognition.interimResults = true;
+  recognition.maxAlternatives = 1;
+  synth = window.speechSynthesis;
+  utter = new SpeechSynthesisUtterance();
 
-const synth = window.speechSynthesis;
+  recognition.onresult = (e) => {
+    let interimTranscript = "";
+    let finalTranscript = "";
+    for (let i = 0; i < e.results.length; ++i) {
+      interimTranscript += e.results[i][0].transcript;
+      if (e.results[i].isFinal) finalTranscript += e.results[i][0].transcript;
+    }
+    youQuery.innerText = interimTranscript;
+    if (finalTranscript) {
+      let transcript = finalTranscript.trim().toLowerCase();
+      handle(transcript);
+    }
+  };
 
-speak.onclick = () => {
-  recognition.start();
+  recognition.onend = () => {
+    start = false;
+    speakButton.style.background = "deepskyblue";
+    speakButton.style.boxShadow = "2px 5px 30px skyblue";
+  };
+
+  speakButton.onclick = () => {
+    clear();
+    start = !start;
+    if (!start) {
+      recognition.abort();
+      return;
+    }
+    speakButton.style.background = "red";
+    speakButton.style.boxShadow = "2px 5px 30px orangered";
+    recognition.start();
+  };
+}
+
+textInput.addEventListener("keyup", (event) => {
+  clear();
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    executeButton.click();
+  }
+});
+
+executeButton.onclick = () => {
+  clear();
+  let transcript = textInput.value.toLowerCase().trim();
+  textInput.value = "";
+  handle(transcript);
 };
 
-const utter = new SpeechSynthesisUtterance();
+function process(data) {
+  const googleKey = "AIzaSyCjDZySq6BsssKXH55PIzZC8IIWz29mFEM";
+  let { intent, query } = data;
+  if (intent === "answer") {
+    if (query) {
+      $.ajax({
+        type: "GET",
+        url: `https://api.wolframalpha.com/v2/query`,
+        dataType: "jsonp",
+        data: {
+          appid: "PUETJ7-7TLY5HG6U3",
+          input: query,
+          output: "json",
+        },
+        success: (response) => {
+          let output = "Sorry, no answer for that.";
+          let queryresult = response.queryresult;
+          if (queryresult.success) {
+            output = queryresult.pods[1].subpods[0].plaintext;
+          }
+          reply(output);
+        },
+        error: (error) => {
+          reply("Request failed.");
+        },
+      });
+      return "Please wait.";
+    }
+  }
+  if (intent === "article") {
+    if (query) {
+      $.ajax({
+        type: "GET",
+        url: `https://en.wikipedia.org/w/api.php`,
+        dataType: "jsonp",
+        data: {
+          action: "query",
+          prop: "extracts",
+          origin: "*",
+          format: "json",
+          generator: "search",
+          gsrsearch: query,
+          gsrnamespace: 0,
+          gsrlimit: 1,
+          explaintext: true,
+          exsentences: 1,
+        },
+        success: (response) => {
+          let pages = response.query.pages;
+          let output = pages[Object.keys(pages)[0]].extract;
+          reply(output);
+        },
+        error: (error) => {
+          reply("Request failed.");
+        },
+      });
+      return;
+    }
+  }
+  if (intent === "weather") {
+    if (query) {
+      $.ajax({
+        type: "GET",
+        url: `https://api.openweathermap.org/data/2.5/weather`,
+        dataType: "jsonp",
+        data: {
+          appid: "8a040940c2f2dbdbf149a1e9b1ba7438",
+          q: query,
+          units: "metric",
+        },
+        success: (response) => {
+          let description = response.weather[0].description;
+          let temp = response.main.temp;
+          let output = `Weather in ${query} is ${description}. Temperature is ${temp}°C.`;
+          reply(output);
+        },
+        error: (error) => {
+          reply("Request failed.");
+        },
+      });
+      return;
+    }
+  }
+  if (intent === "video") {
+    if (query) {
+      $.ajax({
+        type: "GET",
+        url: `https://www.googleapis.com/youtube/v3/search`,
+        dataType: "jsonp",
+        data: {
+          key: googleKey,
+          q: query,
+          part: "snippet",
+          maxResults: 1,
+          type: "video",
+        },
+        success: (response) => {
+          let video = response.items[0];
+          let videoId = video.id.videoId;
+          embeddedDiv.classList.remove("disabled");
+          embeddedDiv.innerHTML = `<iframe class="embed-responsive-item" loading="lazy" allowfullscreen 
+          src="https://www.youtube.com/embed/${videoId}?rel=0&hd=1" style="border:0"></iframe>`;
+          let output = video.snippet.title;
+          reply(output);
+        },
+        error: (error) => {
+          reply("Request failed.");
+        },
+      });
+      return;
+    }
+  }
+  if (intent === "map") {
+    if (query) {
+      embeddedDiv.classList.remove("disabled");
+      embeddedDiv.innerHTML = `<iframe class="embed-responsive-item" loading="lazy" allowfullscreen 
+      src="https://www.google.com/maps/embed/v1/place?key=${googleKey}&q=${query}"></iframe>`;
+      return `Showing ${query}`;
+    }
+  }
+  if (intent === "translate") {
+    if (query) {
+      let obj = destructure(query);
+      $.ajax({
+        type: "GET",
+        url: `https://www.googleapis.com/language/translate/v2`,
+        dataType: "jsonp",
+        data: {
+          key: googleKey,
+          target: obj.intent,
+          q: obj.query,
+        },
+        success: (response) => {
+          let output = response.data.translations[0].translatedText;
+          reply(output);
+        },
+        error: (error) => {
+          reply("Request failed.");
+        },
+      });
+      return;
+    }
+  }
+  if (intent === "search") {
+    if (query) {
+      window.open(`https://www.google.com/search?q=${query}`);
+      return `Searching ${query}`;
+    }
+  }
+  if (intent === "dictionary") {
+    if (query) {
+      $.ajax({
+        type: "GET",
+        url: `https://api.dictionaryapi.dev/api/v2/entries/en_US/${query}`,
+        dataType: "json",
+        success: (response) => {
+          let output = response[0].meanings[0].definitions[0].definition;
+          reply(output);
+        },
+        error: (error) => {
+          reply("Request failed.");
+        },
+      });
+      return;
+    }
+  }
+  if (intent === "joke") {
+    $.ajax({
+      type: "GET",
+      url: `https://official-joke-api.appspot.com/jokes/random`,
+      dataType: "json",
+      success: (response) => {
+        let output = `${response.setup} ${response.punchline}`;
+        reply(output);
+      },
+      error: (error) => {
+        reply("Request failed.");
+      },
+    });
+    return;
+  }
+  if (intent === "location") {
+    if (!navigator.geolocation) return "Geolocation not supported by browser.";
+    navigator.geolocation.getCurrentPosition((position) => {
+      $.ajax({
+        type: "GET",
+        url: `https://open.mapquestapi.com/geocoding/v1/reverse`,
+        dataType: "jsonp",
+        data: {
+          key: "XEf0JKRGAN6eTybaXxFM2GoxZrfmqdaH",
+          location: `${position.coords.latitude},${position.coords.longitude}`,
+        },
+        success: (response) => {
+          let location = response.results[0].locations[0];
+          let output = `${location.street}, ${location.adminArea6}, ${location.adminArea5}, ${location.adminArea4}, ${location.adminArea3}, ${location.adminArea1}`;
+          reply(output);
+        },
+        error: (error) => {
+          reply("Request failed.");
+        },
+      });
+    });
+    return "Please wait.";
+  }
+  if (intent === "time") return new Date().toLocaleTimeString();
+  if (intent === "date") return new Date().toDateString();
+  return "Wrong syntax.";
+}
 
-recognition.onresult = (e) => {
-  const transcript = e.results[e.results.length - 1][0].transcript.trim();
-  you.innerText = transcript;
-  utter.text = reply(transcript.toLowerCase());
-  bot.innerText = utter.text;
+function destructure(transcript) {
+  let data = {};
+  let [intent, ...rest] = transcript.split(" ");
+  data.intent = intent;
+  data.query = rest.join(" ");
+  return data;
+}
+
+function handle(transcript) {
+  let data = destructure(transcript);
+  youIntent.innerText = data.intent;
+  youQuery.innerText = data.query;
+  let output = process(data);
+  reply(output);
+}
+
+function reply(output) {
+  if (!output) return;
+  botAnswer.innerText = output;
+  if (!supported) return;
+  utter.text = output;
   synth.speak(utter);
-};
+  utter.text = "";
+}
 
-function reply(transcript) {
-  if (transcript.includes("hello")) return "Hello sir.";
-  if (transcript.includes("bye")) return "Goodbye sir.";
-  if (transcript.includes("date")) {
-    const event = new Date();
-    const options = {
-      weekday: "long",
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    };
-    return event.toLocaleDateString(undefined, options);
-  }
-  if (transcript.includes("time")) {
-    const event = new Date();
-    return event.getHours() + ":" + event.getMinutes();
-  }
-  if (transcript.includes("search")) {
-    let query = transcript.replace("search", "");
-    window.open(`https://www.google.com/search?q=${query}`);
-    return `Searching ${query}`;
-  }
-  if (transcript.includes("wikipedia")) {
-    let query = transcript.replace("wikipedia", "").trim();
-    let link = `https://en.wikipedia.org/w/api.php?gsrsearch=${query}&action=query&prop=extracts&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=1&exsentences=1&explaintext`;
-    $.ajax({
-      url: link,
-      dataType: "jsonp",
-      success: (response) => {
-        let pages = response.query.pages;
-        let result = pages[Object.keys(pages)[0]].extract;
-        utter.text = result;
-        bot.innerText = utter.text;
-        synth.speak(utter);
-      },
-    });
-    return;
-  }
-  if (transcript.includes("weather")) {
-    let query = transcript.replace("weather", "").trim();
-    let link = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=8a040940c2f2dbdbf149a1e9b1ba7438`;
-    $.ajax({
-      url: link,
-      dataType: "jsonp",
-      success: (response) => {
-        let description = response.weather[0].description;
-        let temp = response.main.temp;
-        let result = `Weather in ${query} is ${description}. Temperature is ${temp} degree Celsius.`;
-        utter.text = result;
-        bot.innerText = utter.text;
-        synth.speak(utter);
-      },
-    });
-    return;
-  }
-  return "I don't understand.";
+function clear() {
+  youIntent.innerText = "";
+  youQuery.innerText = "";
+  botAnswer.innerText = "";
+  embeddedDiv.innerHTML = "";
+  embeddedDiv.classList.add("disabled");
 }
